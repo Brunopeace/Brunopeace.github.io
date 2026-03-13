fbb8420a8bab6b2f9b364bfdfa43deead2bb5a7b
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-03-12 22:08:19 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 self.addEventListener('install', (event) => {
   self.skipWaiting(); // força ativação imediata
 
-  const CACHE_NAME = 'gerenciador-de-clientes-v34';
+  const CACHE_NAME = 'gerenciador-de-clientes-v35';
   const urlsToCache = [
       '/',
       '/index.html',
